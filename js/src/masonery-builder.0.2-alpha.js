8f21014b52043d010fccb023b22cbeba205c1980
plisken1997 <raphael.bacconnier@yahoo.fr>
2015-04-24 22:35:17 +0200

    refactorisation

--- js/src/masonery-builder.0.2-alpha.js
+++ js/src/masonery-builder.0.2-alpha.js
@@ -1,192 +1,27 @@
 (function ($, ctxt) {
 
     "use strict";
-    
-    $.fn.masoneryBuilder = function (opts) {
 
-        var self = this,
-            nbCols = 4,
-            marginTop = 20,
-            marginRight = 20,
-            colWidth = Math.floor(self.width() / nbCols) - marginRight,
-            options = $.extend({
-                eventNamespace : "masonery_builder",
-                debug: false,
-                marginTop: marginTop,
-                marginRight: marginRight,
-                nbCols: nbCols,
-                colWidth: colWidth,
-                dragenter: function () {},
-                dragleave: function () {},
-                dragstop: function () {},
-                dragover: function () {}
-            }, opts),
-            isBuilding = false;
+    var DI = {};
 
-        function logWarning(msg) {
+    (function ($, DI) {
+
+        function logWarning(msg, options) {
             if (options.debug) {
                 console.warn("masonery-builder: "+msg);
             }
         }
 
-        function log(msg) {
+        function log(msg, options) {
             if (options.debug) {
                 console.log("masonery-builder: "+msg);
             }
         }
 
-        function logError(msg) {
+        function logError(msg, options) {
             if (options.debug) {
                 console.error("masonery-builder: "+msg);
             }
-        }
-
-        function triggerEvent(eventName, datas) {
-            var d = datas || {};
-            self.trigger(options.eventNamespace + '.' + eventName, {
-                target: self,
-                datas: d
-            });
-        }
-
-        $(this)
-            .on('dragenter', function () {
-                triggerEvent("dropTarget:dragenter", arguments);
-                options.dragenter(event, this, self);
-            })
-            .on('dragleave', function () {
-                triggerEvent("dropTarget:dragleave", arguments);
-                options.dragleave(event, this, self);
-            })
-            .on('dragstop', function (event) {
-                triggerEvent("dropTarget:dragstop", arguments);
-                options.dragstop(event, this, self);
-            })
-            .on('dragover', function (event) {
-                triggerEvent("dropTarget:dragover", arguments);
-                options.dragover(event, this, self);
-                event.preventDefault();
-            })
-            .on('drop', function (event) {
-                var util = {
-                    computeLayerPosition: function(event, container) {
-                        var $elt = $(event.target);
-                        if ($elt.hasClass("gallery")) {
-                            return {
-                                x: event.layerX,
-                                y: event.layerY
-                            }
-                        } else if (!$elt.hasClass("item")) {
-                            $elt = $elt.parent(".item");
-                        }
-                            
-                        return {
-                            x: $elt.position().left + event.layerX,
-                            y: $elt.position().top + event.layerY
-                        }
-                    },
-
-                    findContainer: function(container) {
-                        if($(container).hasClass("gallery")) {
-                            return $(container);
-                        }
-                        return $(container).parent(".gallery");
-                    }
-                };
-
-                triggerEvent("dropTarget:drop", arguments);
-                var $container = util.findContainer(event.delegateTarget),
-                    ogEvent = event.originalEvent,
-                    dataTransfer = JSON.parse(ogEvent.dataTransfer.getData('text/json')),
-                    coords = util.computeLayerPosition(ogEvent, $container);
-                coords.x = coords.x - dataTransfer.layer.x;
-                coords.y = coords.y - dataTransfer.layer.y;
-                if(0 > coords.x) {
-                    coords.x = 0;
-                }
-                if(0 > coords.y) {
-                    coords.y = 0;
-                }
-                self.trigger(options.eventNamespace+'.dropTarget:process_drop', {
-                    pos: coords, 
-                    id: dataTransfer.id
-                });
-            })
-            .on(options.eventNamespace+'.dropTarget:process_drop', function (event, datas) {
-                placeItem(datas.pos, datas.id);
-            })
-            .on(options.eventNamespace+".masonery_ends", function (sender, datas) {
-                var maxH = self.height();
-                self.height(maxH);
-            })
-            .on(options.eventNamespace+".item_placed", function () {
-            })
-            .find('[draggable="true"]')
-                .masoneryBuilderDraggable();
-
-
-        function placeItem(pos, id) {
-            $('#'+id)
-                .stop()                
-                .animate({
-                    left: pos.x, 
-                    top: pos.y
-                },{
-                    complete: function () {
-                        self.trigger(options.eventNamespace+".item_placed");
-                }});
-
-                self.trigger(options.eventNamespace+".item_placed");
-        }
-
-        function build() {
-            var cols = [], maxH = 0, nextH, i, j, l;
-            for (i = 0; i < options.nbCols; i++) {
-                cols.push([]);
-            }
-
-            self.find(".item").each(function (elt, pos) {
-                i = findColumn($(this).position().left);
-                if (null !== i) {
-                    cols[i].push({
-                        $elt : $(this),
-                        x: getColumns()[i].start,
-                        y: null
-                    });
-                }
-            });
-
-            for (i = 0; i < options.nbCols; i++) {
-                cols[i] = cols[i].sort(function (a, b) {
-                    return a.$elt.position().top < b.$elt.position().top ? -1 : 1;
-                });
-                nextH = 0;
-                /*
-                if (0  < i && cols[i].length > 0) {
-                    nextH = computeHandlePreviousColumn(cols[i][0], nextH, cols[i-1]);
-                }*/
-                for (j = 0, l = cols[i].length; j < l; j++) {
-                    if (0  < i) {
-                        nextH = computeHandlePreviousColumn(cols[i][j], nextH, cols[i-1]);
-                    }       
-                    cols[i][j].y = nextH;
-                    nextH += cols[i][j].$elt.height() + options.marginTop;
-                }
-                if (nextH > maxH) {
-                    maxH = nextH;
-                }
-            }
-            self.stop().animate({height: maxH});
-            cols.forEach(function(e,i){
-                e.forEach(function(item, j){
-                    item.$elt.stop().animate({
-                        top: item.y,
-                        left: item.x,
-                    }, {
-                        easing: "linear"
-                    });
-                });
-            });
         }
 
         function collision(left, right, targetTop) {
@@ -205,11 +40,10 @@
             ) {
                 return true;
             }
-            // @todo implémenter la colision
             return false;
         }
 
-        function computeHandlePreviousColumn(elt, targetTop, previousCol) {
+        function computeHandlePreviousColumn(elt, targetTop, previousCol, options) {
             for( var i = 0, l = previousCol.length; i < l; i++) {
                 if (collision(previousCol[i], elt, targetTop)) {
                     targetTop = previousCol[i].y + previousCol[i].$elt.height() + options.marginTop;
@@ -218,19 +52,19 @@
             return targetTop;
         }
 
-        function getColumns(x) {
+        function getColumns(options) {
             var cols = [],
                 i;
             for (i = 0; i < options.nbCols; i++) {
-                cols.push(getOffsetForColumn(i));
+                cols.push(getOffsetForColumn(i, options));
             }
             return cols;
         }
 
-        function findColumn(x) {
+        function findColumn(x, options) {
             var col, i;
             for (i = 0; i < options.nbCols; i++) {
-                col = getOffsetForColumn(i);
+                col = getOffsetForColumn(i, options);
                 if (col.start <= x && x <= col.end) {
                     return i;
                 }
@@ -238,7 +72,7 @@
             return null;
         }
 
-        function getOffsetForColumn(pos) {
+        function getOffsetForColumn(pos, options) {
             return {
                 start: (pos * (options.colWidth + options.marginRight)),
                 end: ((pos+1) * (options.colWidth + options.marginRight)) - 1
@@ -246,7 +80,215 @@
         }
 
 
-        this.build = build;
+        function MasoneryBuilder($elt, options) {
+            this.$elt = $elt;
+            this.options = options;
+            this.isBuilding = false;
+        }
+
+        MasoneryBuilder.prototype = {
+
+            placeItem: function (pos, id) {
+                var $elt = this.$elt,
+                    options = this.options;
+                $('#'+id)
+                    .stop()
+                    .animate({
+                        left: pos.x,
+                        top: pos.y
+                    },{
+                        complete: function () {
+                            $elt.trigger(options.eventNamespace+".item_placed");
+                    }});
+                return this;
+            },
+
+            build: function () {
+                if (this.isBuilding) {
+                    logWarning("currently building", this.options);
+                    return this;
+                }
+                var cols = [],
+                    maxH = 0,
+                    added = [],
+                    self = this,
+                    nextH, i, j, l;
+                this.isBuilding = true;
+                for (i = 0; i < this.options.nbCols; i++) {
+                    cols.push([]);
+                }
+
+                this.$elt.find(".item").each(function (elt, pos) {
+                    i = findColumn($(this).position().left, self.options);
+                    if (null !== i) {
+                        cols[i].push({
+                            $elt : $(this),
+                            x: getColumns(self.options)[i].start,
+                            y: null
+                        });
+                    }
+                });
+
+                for (i = 0; i < this.options.nbCols; i++) {
+                    cols[i] = cols[i].sort(function (a, b) {
+                        return a.$elt.position().top < b.$elt.position().top ? -1 : 1;
+                    });
+                    nextH = 0;
+                    for (j = 0, l = cols[i].length; j < l; j++) {
+                        if (0  < i) {
+                            nextH = computeHandlePreviousColumn(cols[i][j], nextH, cols[i-1], this.options);
+                        }
+                        cols[i][j].y = nextH;
+                        nextH += cols[i][j].$elt.height() + self.options.marginTop;
+                    }
+                    if (nextH > maxH) {
+                        maxH = nextH;
+                    }
+                }
+                this.$elt.stop().animate({height: maxH});
+                cols.forEach(function(e,i){
+                    e.forEach(function(item, j){
+                        added.push(1);
+                        item.$elt.stop().animate({
+                            top: item.y,
+                            left: item.x,
+                        }, {
+                            easing: "linear",
+                            complete: function () {
+                                added.pop();
+                                if (0 === added.length) {
+                                    self.triggerEvent("masonery_ends");
+                                }
+                            }
+                        });
+                    });
+                });
+            },
+
+            triggerEvent: function (eventName, datas) {
+                var d = datas || {};
+                this.$elt.trigger(this.options.eventNamespace + '.' + eventName, {
+                    target: self,
+                    datas: d
+                });
+            }
+        };// end prototype
+
+        DI.createMasoneryBuilder = function ($elt, opts) {
+            return new MasoneryBuilder($elt, opts);
+        };
+
+    }($, DI));
+
+
+    $.fn.masoneryBuilder = function (opts) {
+
+        var nbCols = 4,
+            marginTop = 20,
+            marginRight = 20,
+            options = $.extend({
+                eventNamespace : "masonery_builder",
+                debug: false,
+                marginTop: marginTop,
+                marginRight: marginRight,
+                nbCols: nbCols,
+                dragenter: function () {},
+                dragleave: function () {},
+                dragstop: function () {},
+                dragover: function () {}
+            }, opts),
+            isBuilding = false;
+
+
+        $(this).each(function (elt, pos) {
+
+            var self = $(this),
+                opts = $.extend(options, {
+                    colWidth: Math.floor($(this).width() / nbCols) - marginRight,
+                }),
+                builder = DI.createMasoneryBuilder($(this), opts);
+
+            $(this)
+                .on('dragenter', function (event) {
+                    builder.triggerEvent("dropTarget:dragenter", arguments);
+                    opts.dragenter(event, this, self);
+                })
+                .on('dragleave', function (event) {
+                    builder.triggerEvent("dropTarget:dragleave", arguments);
+                    opts.dragleave(event, this, self);
+                })
+                .on('dragstop', function (event) {
+                    builder.triggerEvent("dropTarget:dragstop", arguments);
+                    opts.dragstop(event, this, self);
+                })
+                .on('dragover', function (event) {
+                    builder.triggerEvent("dropTarget:dragover", arguments);
+                    opts.dragover(event, this, self);
+                    event.preventDefault();
+                })
+                .on('drop', function (event) {
+                    var util = {
+                        computeLayerPosition: function(event, container) {
+                            var $elt = $(event.target);
+                            if ($elt.hasClass("gallery")) {
+                                return {
+                                    x: event.layerX,
+                                    y: event.layerY
+                                }
+                            } else if (!$elt.hasClass("item")) {
+                                $elt = $elt.parent(".item");
+                            }
+
+                            return {
+                                x: $elt.position().left + event.layerX,
+                                y: $elt.position().top + event.layerY
+                            }
+                        },
+
+                        findContainer: function(container) {
+                            if($(container).hasClass("gallery")) {
+                                return $(container);
+                            }
+                            return $(container).parent(".gallery");
+                        }
+                    };
+
+                    builder.triggerEvent("dropTarget:drop", arguments);
+                    var $container = util.findContainer(event.delegateTarget),
+                        ogEvent = event.originalEvent,
+                        dataTransfer = JSON.parse(ogEvent.dataTransfer.getData('text/json')),
+                        coords = util.computeLayerPosition(ogEvent, $container);
+                    coords.x = coords.x - dataTransfer.layer.x;
+                    coords.y = coords.y - dataTransfer.layer.y;
+                    if(0 > coords.x) {
+                        coords.x = 0;
+                    }
+                    if(0 > coords.y) {
+                        coords.y = 0;
+                    }
+                    self.trigger(opts.eventNamespace+'.dropTarget:process_drop', {
+                        pos: coords,
+                        id: dataTransfer.id
+                    });
+                })
+                .on(opts.eventNamespace+'.dropTarget:process_drop', function (event, datas) {
+                    builder.placeItem(datas.pos, datas.id);
+                })
+                .on(opts.eventNamespace+".masonery_ends", function (sender, datas) {
+                    builder.isBuilding = false;
+                })
+                .on(opts.eventNamespace+".item_placed", function () {
+                })
+                .find('[draggable="true"]')
+                    .masoneryBuilderDraggable();
+
+            $(this).addClass("masonery-builder");
+            this.doBuild = builder.build.bind(builder);
+        });
+
+        this.build = function () {
+            this[0].doBuild();
+        };
 
         return this;
     }
